eb6293872c9ac7d67e9359f5fc93eb7c54d7d66c
kumarguptavaibhav <vaibhav9161porwal@gmail.com>
2025-08-24 16:16:44 +0530

    Update bcrypt functionality in signin and signup controller

--- Backend/controllers/users.controllers.js
+++ Backend/controllers/users.controllers.js
@@ -1,4 +1,5 @@
 const Users = require("../models/users.models");
+const bcrypt = require("bcrypt");
 
 const createUser = async (req, res) => {
   try {
@@ -18,12 +19,18 @@ const createUser = async (req, res) => {
       err.statusCode = 409;
       throw err;
     }
-    const create_result = await Users.create({
-      name: name,
-      email: email,
-      password: password,
+    const saltRound = 10;
+    bcrypt.hash(password, saltRound, async (err, hash) => {
+      if (err) {
+        throw err;
+      }
+      const create_result = await Users.create({
+        name: name,
+        email: email,
+        password: hash,
+      });
+      res.status(201).json({ error: false, data: "User Created Successfully" });
     });
-    res.status(201).json({ error: false, data: "User Created Successfully" });
   } catch (error) {
     res
       .status(error.statusCode || 500)
@@ -50,13 +57,13 @@ const signInUser = async (req, res) => {
       err.statusCode = 404;
       throw err;
     }
-    if (password === check_user.password) {
-      res.status(200).json({ error: false, data: "User Login Successfully" });
-    } else {
+    const isMatch = await bcrypt.compare(password, check_user.password);
+    if (!isMatch) {
       let err = new Error("User not authorized");
       err.statusCode = 401;
       throw err;
     }
+    res.status(200).json({ error: false, data: "User Login Successfully" });
   } catch (error) {
     res
       .status(error.statusCode || 500)
